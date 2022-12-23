305337eb528babde47fa3b274f67eeec7fac3501
korotkovak <kristinakorotkova059@gmail.com>
2022-12-23 18:16:30 +0800

    Add background

--- TomatoMethodApp/ViewController.swift
+++ TomatoMethodApp/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI Elements
+
+    private lazy var imageBackground: UIImageView = {
+        let width = UIScreen.main.bounds.size.width
+        let height = UIScreen.main.bounds.size.height
+        let imageView = UIImageView(frame: CGRect(x:0, y:0, width: width, height: height))
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleAspectFill
+        imageView.center = view.center
+        return imageView
+    }()
+
+
+
+
+
+    // MARK: - Leficycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
+        setupHierarchy()
     }
+
+    private func setupHierarchy() {
+        view.addSubviews([
+            imageBackground
+        ])
+    }
+
+    // MARK: - Setup
+
+    // MARK: - Actions
 
 
 }
 
+extension UIView {
+    func addSubviews(_ subviews: [UIView]) {
+        subviews.forEach { addSubview($0) }
+    }
+}
